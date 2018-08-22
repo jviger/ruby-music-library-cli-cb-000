66a26c5a6a0a474c6d2eb05f632ccfd3e84064aa
Jason Viger <jason.viger@gmail.com>
2018-08-22 05:40:23 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -14,7 +14,8 @@ class MusicLibraryController
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the genres in your library, enter 'list genres'."
     puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'"
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
+    puts "To play a song, enter 'play song'."
     gets
   end
   
