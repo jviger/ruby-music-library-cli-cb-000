cc2e4468f202ae9735557639a7d948759ab6b736
Jason Viger <jason.viger@gmail.com>
2018-08-22 06:03:46 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -50,9 +50,9 @@ class MusicLibraryController
   end
   
   def list_artists
-    song_list =Song.all.sort{|x,y| x.name <=> y.name }
-    song_list.each do |x|
-    puts "#{song_list.index(x) + 1}. #{x.artist.name} - #{x.name} - #{x.genre.name}"
+    artist_list =Artist.all.sort{|x,y| x.name <=> y.name }
+    artist_list.each do |x|
+    puts "#{x}"
     end
   end
   
